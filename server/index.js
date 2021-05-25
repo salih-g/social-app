0d316b5a608990f6b566f7519504ecaa3392e39f
Salih <sudanmerinosu@gmail.com>
2021-05-26 00:19:20 +0300

    First api route

--- server/index.js
+++ server/index.js
@@ -1,12 +1,38 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
+import dotenv from 'dotenv';
 import cors from 'cors';
 
+import postRoutes from './routes/posts.js';
+import apiRoutes from './routes/api.js';
+
+dotenv.config();
+
 const app = express();
+
+app.use('/api', apiRoutes);
+app.use('/api/posts', postRoutes);
 
 app.use(bodyParser.json({ limit: '30mb', extended: true }));
 app.use(
 	bodyParser.urlencoded({ limit: '30mb', extended: true })
 );
 app.use(cors({ origin: '*' }));
+
+const PORT = process.env.PORT || 5000;
+
+mongoose
+	.connect(process.env.MONGO_URL, {
+		useNewUrlParser: true,
+		useUnifiedTopology: true,
+	})
+	.then(() =>
+		app.listen(PORT, () => {
+			console.log('DB Connected!');
+			console.log(`Server running on port: ${PORT}`);
+		})
+	)
+	.catch((err) => console.err(err.message));
+
+mongoose.set('useFindAndModify', false);
