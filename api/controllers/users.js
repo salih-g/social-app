d0c62bc719a5995cfa943b529edaeefdc61b51cf
Salih <sudanmerinosu@gmail.com>
2021-05-29 00:37:16 +0300

    WebClient: Jwt Login added

---
+++ api/controllers/users.js
@@ -0,0 +1,8 @@
+import bcrypt from 'bcryptjs';
+import jwt from 'jsonwebtoken';
+
+import User from '../models/User.js';
+
+export const signin = async (req, res) => {};
+
+export const signup = async (req, res) => {};
--- api/index.js
+++ api/index.js
@@ -5,6 +5,7 @@ import dotenv from 'dotenv';
 import cors from 'cors';
 
 import postRoutes from './routes/posts.js';
+import userRoutes from './routes/users.js';
 import apiRoutes from './routes/api.js';
 
 dotenv.config();
@@ -12,13 +13,12 @@ dotenv.config();
 const app = express();
 
 app.use(bodyParser.json({ limit: '30mb', extended: true }));
-app.use(
-	bodyParser.urlencoded({ limit: '30mb', extended: true })
-);
+app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }));
 app.use(cors({ origin: '*' }));
 
 app.use('/api', apiRoutes);
 app.use('/api/posts', postRoutes);
+app.use('./api/user', userRoutes);
 
 const PORT = process.env.PORT || 5000;
 
