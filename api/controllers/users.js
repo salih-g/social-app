14b3f4b2500001660ef72197ca24d51709f4607f
Salih <sudanmerinosu@gmail.com>
2021-05-29 01:09:15 +0300

    Api: Jwt auth added

--- api/controllers/users.js
+++ api/controllers/users.js
@@ -1,8 +1,86 @@
 import bcrypt from 'bcryptjs';
 import jwt from 'jsonwebtoken';
+import dotenv from 'dotenv';
 
 import User from '../models/User.js';
 
-export const signin = async (req, res) => {};
+dotenv.config();
 
-export const signup = async (req, res) => {};
+export const signin = async (req, res) => {
+	const { email, password } = req.body;
+
+	try {
+		const existingUser = await User.findOne({ email });
+
+		if (!existingUser)
+			return res
+				.status(404)
+				.json({ message: 'User does not exist.' });
+
+		const isPasswordCorrect = await bcrypt.compare(
+			password,
+			existingUser.password
+		);
+
+		if (!isPasswordCorrect)
+			return res
+				.status(400)
+				.json({ message: 'Invalid credentials.' });
+
+		const token = jwt.sign(
+			{
+				email: existingUser.email,
+				id: existingUser._id,
+			},
+			process.env.SECRET,
+			{
+				expiresIn: '1h',
+			}
+		);
+
+		return res.status(200).json({ result: existingUser, token });
+	} catch (err) {
+		console.error(err);
+		return res.status(500).json({ message: 'Something went wrong!' });
+	}
+};
+
+export const signup = async (req, res) => {
+	const { email, password, confirmPassword, firstName, lastName } =
+		req.body;
+
+	try {
+		const existingUser = await User.findOne({ email });
+
+		if (existingUser)
+			return res.status(400).json({ message: 'User already exist.' });
+
+		if (password !== confirmPassword)
+			return res
+				.status(400)
+				.json({ message: "Passwords don't match." });
+
+		const hashedPassword = await bcrypt.hash(password, 12);
+
+		const result = await User.create({
+			email,
+			password: hashedPassword,
+			name: `${firstName} ${lastName}`,
+		});
+
+		const token = jwt.sign(
+			{
+				email: result.email,
+				id: result._id,
+			},
+			process.env.SECRET,
+			{
+				expiresIn: '1h',
+			}
+		);
+
+		return res.status(200).json({ result, token });
+	} catch (err) {
+		console.error(err);
+	}
+};
